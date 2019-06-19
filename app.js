e4dfd4360b092828cd18acadf8fb18d03fcc77e6
Diego Sastoque <diego.sastoque@smartmatic.com>
2019-06-19 16:44:20 -0500

    header done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// settings
+
+app.use(express.static("public"));
+app.set("views", "./src/views");
+app.locals.basedir = path.join(__dirname, "views");
+app.set("view engine", "pug");
+
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${ port }`);
+});
