24184f4fa46d99c82a88d46676fba5813cdea6a7
diegobeltran97 <diegoelcairo@gmail.com>
2019-06-24 08:30:13 -0500

    dashboard done

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -1,15 +1,30 @@
-var ctx = document.getElementsByClassName("myChart").getContext('2d');
-document.getElementsByClassName
-var myChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    datasets: [{
-      backgroundColor: [
-        "#2ecc71",
-        "#3498db",
-        "#95a5a6",
-      ],
-      data: [12, 19, 3]
-    }]
-  }
-});
+(function(Window) {
+  var App = {
+    htmlElements: {
+      cardProgress: document.getElementsByClassName("myChart-js"),
+      cardActivty: document.getElementsByClassName('card-activity')
+    },
+
+    graph: function() {
+      Array.from(App.htmlElements.cardProgress).map(element => {
+        let myChart = new Chart(element, {
+          type: "pie",
+          data: {
+            datasets: [
+              {
+                backgroundColor: ["#2ecc71", "#3498db", "#95a5a6"],
+                data: [12, 19, 3]
+              }
+            ]
+          }
+        });
+      });
+    },
+
+    init: function() {
+      console.log("inicio");
+      App.graph();
+    }
+  };
+  Window.App = App;
+})(window);
