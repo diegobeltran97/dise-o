6c5debe3b997640210c642b622a3254f481921b3
diego.sastoque <diego.sastoque@smartmatic.com>
2019-07-31 17:23:28 -0500

    update dashboard

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -2,9 +2,10 @@
   var App = {
     htmlElements: {
       cardProgress: document.getElementsByClassName("myChart-js"),
+      cardPolling:  $('.cardpolling'),
       btncardPolling: $('.card-parent'),
-      btnoption: $('.btn-option-js'),
-      btnState: $('#btn-state-js'),
+      btnSteps: $('.btn-steps'),
+      btnState: $('.btn-state-js'),
       options: $('#options')
 
       
@@ -23,32 +24,59 @@
     dashboard: function(){
       let opcion = Array.from( App.htmlElements.options.children());
       // Default status of steps
-      
+      App.dashboardCardPolling();
 
-      opcion.forEach(function(element , index ) {
-        let btn  = element.querySelectorAll('#btn-state-js');
-        
-    
-        // if ( element.getAttribute("status") == "false" ) {
-        //       element.setAttribute("style", "display:none;");
-        //  }
+      opcion.forEach(function(element, index ) {
+        var btn = element.querySelector('.btn-state-js');
+        var i = index;
+        if ( btn != null) {
+          btn.setAttribute("step", i);
+          element.setAttribute("step", i);
+          
+        }
+      
+        if ( element.getAttribute("status") == "false" ) {
+              element.setAttribute("style", "display:none;");
+          }
           
       })
       //Dinamyc Steps
       let options =  App.htmlElements.options.children();
-      App.htmlElements.btnState.click( function () {
-
+      App.htmlElements.btnState.click( function (e) {
+         var btnSelected = e.target;
+         App.nextStep(btnSelected.getAttribute("step"))
       })
+
       App.htmlElements.btncardPolling.click( function() {
-        $(".collapse").collapse('toggle');
-        $("#option-1").toggle();
-        $("#option-2").toggle();
+     
+        $("#options").toggle();
+    
       })
 
-      App.htmlElements.btnoption.click( function() {
-        $("#option-2").toggle();
-        $("#option-1").toggle();
+      App.htmlElements.btnSteps.click( function() {
+        $("#options").show();
+        App.nextStep(0);   
+       
       })
+
+
+    },
+
+    dashboardCardPolling: function () {
+
+      App.htmlElements.cardPolling.each(function(index) {
+        $(".card-parent").attr("data-toggle","collapse")
+        $(".card-parent").attr("data-target",index)
+        $(".card-header").attr("class",index)
+        $(".collapser").attr("data-parent",index)
+      });
+    },
+
+    nextStep: function( n){
+      var i = parseInt(n);
+      var opcion = Array.from( App.htmlElements.options.children());
+       opcion[i].setAttribute("style", "display:none;");
+       opcion[i + 1].setAttribute("style", "display:inline");
     },
 
     graph: function() {
